41146f9771ea6932a1661cf123fa88141e01e84d
이아현 <ahzzng@naver.com>
2024-01-25 20:39:15 +0900

    algorithm : 스택큐_같은 숫자는 싫어

---
+++ StackAndQueue/P12906.java
@@ -0,0 +1,23 @@
+package StackAndQueue;
+
+import java.util.ArrayList;
+
+public class P12906 {
+
+    public static int[] solution(int []arr) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int cur = -1;
+        for(int i=0;i<arr.length;i++){
+
+            if(cur!=arr[i]){
+                arrayList.add(arr[i]);
+                cur = arr[i];
+            }
+
+        }
+        return arrayList.stream().mapToInt(i->i).toArray();
+    }
+    public static void main(String[] args) {
+
+    }
+}
