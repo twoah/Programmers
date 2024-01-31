18ba16f8917e91c9d8586eb7d56f766dda7a317a
ahhyeonlee <ahzzng@naver.com>
2024-01-31 09:49:13 +0900

    algorithm : DP_징검다리

---
+++ Softeer/DP/P6293.java
@@ -0,0 +1,25 @@
+package Softeer.DP;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/** 소프티어 징검다리 문제**/
+public class P6293 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] rocks = IntStream.range(0, N).map( i -> scanner.nextInt()).toArray();
+        int[] dp = IntStream.range(0, N).map(i -> 1).toArray();
+
+        for(int i=0;i<N;i++){
+            for(int j=i;j>0;j--){
+                if(rocks[i] > rocks[j]){
+                    dp[i] = Math.max(dp[i], dp[j] +1);
+                }
+            }
+        }
+        System.out.println(dp[N-1]);
+    }
+}
